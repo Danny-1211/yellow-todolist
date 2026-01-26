8c8a3ee1fc0f61dccbafef0066a729b8b0d4a981
danny <danny21316@gmail.com>
2026-01-26 14:55:23 +0800

    feat: install React-toastify and create global component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useRoutes } from 'react-router-dom'
 import routes from './router/index.jsx';
+import ToastManager from './components/ToastManager.jsx';
 function App() {
   const element = useRoutes(routes)
   return (
     <>
       {element}
+      <ToastManager />
     </>
   )
 }
